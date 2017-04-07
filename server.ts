4bc1486b15c7212c2ab99426bf94dd9972a533e3
Lukasz Momot <lukasz.momot@sdx-ag.de>
2017-04-07 19:18:30 +0200

    yql integration

--- server.ts
+++ server.ts
@@ -1,8 +1,12 @@
+import { StocksService } from "./src/businessServices/stocksService";
 import { Greeter } from "./src/greeter";
 
 const greeter = new Greeter("Lukas");
 
 const msg: string = greeter.greet();
+
+const stocksSrv = new StocksService();
+stocksSrv.getCurrentStockPrice();
 
 console.log("Server Started...");
 console.log(`Hello ${msg}`);
--- src/businessServices/stocksService.ts
+++ src/businessServices/stocksService.ts
@@ -1,9 +1,19 @@
 import { StockPrice } from "../models/stockPrice";
+
+import * as yql from "yql";
+
 /**
  * StocksService
  */
 export class StocksService {
-  public getCurrentStockPrice(): StockPrice {
-      return new StockPrice("Vonovia", 35);
-   }
+    public getCurrentStockPrice(): StockPrice {
+        yql('select * from yahoo.finance.quote where symbol in ("YHOO","AAPL","GOOG","MSFT")', {
+            env: "store://datatables.org/alltableswithkeys",
+        })
+            .exec((error, response) => {
+                console.log(error.message);
+            });
+
+        return new StockPrice("Vonovia", 35);
+    }
 }
