24503eefc9f830cbe54dc159e11e101468bd385c
Lukasz Momot <lukasz.momot@sdx-ag.de>
2017-04-07 17:31:08 +0200

    Enable debug

--- server.ts
+++ server.ts
@@ -1,1 +1,9 @@
-console.log('Hello Server');
+import { Greeter } from "./src/greeter";
+
+const greeter = new Greeter("Lukas");
+
+const msg: string = greeter.greet();
+
+console.log("Server Started...");
+console.log(`Hello ${msg}`);
+
