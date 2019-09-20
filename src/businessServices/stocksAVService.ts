b2259c4e2160f824c32d5d98886aeaea9376385f
Lukas Momot <lukasz.momot@hotmail.com>
2019-09-20 15:56:18 +0200

    changes to AlphantageService and tslint

--- src/businessServices/stocksAVService.ts
+++ src/businessServices/stocksAVService.ts
@@ -1,4 +1,3 @@
-import * as alpha from "alphavantage";
 import * as _ from "lodash";
 import * as unirest from "unirest";
 
@@ -6,7 +5,7 @@ import { StockPrice } from "../models/stockPrice";
 
 export class StocksAVService {
 
-    public getCurrentStockPrice(symbol: string): Promise<any> {
+    public getCurrentStockPrice(symbol: string): Promise<StockPrice> {
         const promise = new Promise<any>((resolve, reject) => {
             unirest.get("https://www.alphavantage.co/query")
                 .query({
@@ -24,10 +23,14 @@ export class StocksAVService {
                     let stock: StockPrice;
 
                     try {
-                        // TODO: Improve this code (done quickly after suddenly Yahoo finance was switched off)
-                        const first = Object.keys(response.body["Time Series (1min)"])[0];
-                        const priceEntry = response.body["Time Series (1min)"][first];
-                        currentPrice = _.toNumber(priceEntry["4. close"]);
+                        if (response && response.body && !response.body.Note) {
+                            const first = Object.keys(response.body["Time Series (1min)"])[0];
+                            const priceEntry = response.body["Time Series (1min)"][first];
+                            currentPrice = _.toNumber(priceEntry["4. close"]);
+                        } else if (response && response.body.Note) {
+                            // There is a limit of calling alphavanted API
+                            reject(response.body.Note);
+                        }
                     } catch (error) {
                         console.log(error);
                         reject(error);
