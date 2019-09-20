cd97d7540d46fc57907c21b4711e031dbef96029
Lukas Momot <lukasz.momot@hotmail.com>
2019-09-20 16:02:15 +0200

    cleanup of stock price API

--- src/api/stockPriceApi.ts
+++ src/api/stockPriceApi.ts
@@ -1,8 +1,5 @@
-import * as promiseRetry from "promise-retry";
-import { StocksService } from "../businessServices/stocksService";
-import { StockPrice } from "../models/stockPrice";
-
 import { NextFunction, Request, Response, Router } from "express";
+import * as promiseRetry from "promise-retry";
 import { StocksAVService } from "../businessServices/stocksAVService";
 
 /**
@@ -23,8 +20,8 @@ export class StockPriceApi {
         const symbol: string = req.params.symbol;
 
         const stockServie = new StocksAVService();
+        // TODO: remove this promise retry because the error is caused by Alphavanted API limitations
         promiseRetry({ retries: 5,  maxTimeout: 300, minTimeout: 100  }, (retry, nr) => {
-            // tslint:disable-next-line:no-console
             console.log("attempt number", nr);
 
             return stockServie.getCurrentStockPrice(symbol.toUpperCase()).catch(retry);
@@ -32,28 +29,15 @@ export class StockPriceApi {
     }
 
     public getMostTraded(req: Request, res: Response, next: NextFunction) {
-        const mostTradedStocks = '"BMW.DE", "BAYN.DE", "SIE.DE", "ALV.DE", "ADS.DE"';
-
-        // tslint:disable-next-line:max-line-length
         const fallbackData = [{ change: -0.08, date: "2017-08-28T19:05:27.121Z", price: "61.25", symbol: "ATVI", name: "Activision Blizzard" },
-                             // tslint:disable-next-line:max-line-length
                              { change: -0.05, date: "2017-08-28T19:05:27.121Z", price: "46.62", symbol: "INTC", name: "Intel Corp." },
-                             // tslint:disable-next-line:max-line-length
                              { change: 0.05, date: "2017-08-28T19:05:27.121Z", price: "84.43", symbol: "MSFT", name: "Microsoft Corp." },
-                             // tslint:disable-next-line:max-line-length
                              { change: -0.85, date: "2017-08-28T19:05:27.121Z", price: "174.76", symbol: "AAPL", name: "Apple Inc." },
                              { change: -2.4, date: "2017-08-28T19:05:27.121Z", price: "209.25", symbol: "NVDA",
                                 name: "Nvidia Corporation" }];
 
-        // TODO: Mock-Data done for Yahoo finance problems
+        // IMPORTANT: Data is mocked because Alphavanted has a limit of 5 requests per minute.
+        // So we are sending just mocked data
         res.send(fallbackData);
-
-        // const stockServie = new StocksService();
-        // promiseRetry({ retries: 20,  maxTimeout: 150, minTimeout: 100 }, (retry, nr) => {
-        //     // tslint:disable-next-line:no-console
-        //     console.log("attempt number", nr);
-
-        //     return stockServie.getCurrentStockPrices(mostTradedStocks).catch(retry);
-        // }).then((price) => res.send(price), (error) => res.send(fallbackData));
     }
 }
