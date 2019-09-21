5cb2262395bc58b26638a0fc457d9a4b71c9e0a0
Lukas Momot <lukasz.momot@hotmail.com>
2019-09-21 20:07:32 +0200

    add http caching

--- src/api/stockPriceApi.ts
+++ src/api/stockPriceApi.ts
@@ -21,20 +21,27 @@ export class StockPriceApi {
 
         const stockServie = new StocksAVService();
         // TODO: remove this promise retry because the error is caused by Alphavanted API limitations
-        promiseRetry({ retries: 5,  maxTimeout: 300, minTimeout: 100  }, (retry, nr) => {
+        promiseRetry({ retries: 5, maxTimeout: 300, minTimeout: 100 }, (retry, nr) => {
             console.log("attempt number", nr);
 
             return stockServie.getCurrentStockPrice(symbol.toUpperCase()).catch(retry);
-        }).then((price) => res.send(price), (error) => res.status(500).send({ error }));
+        }).then((price) => {
+            // Set http cache to 1 minute for the browsers/clients
+            res.setHeader("Cache-Control", "max-age=60");
+            res.send(price);
+        },
+            (error) => res.status(500).send({ error }));
     }
 
     public getMostTraded(req: Request, res: Response, next: NextFunction) {
         const fallbackData = [{ change: -0.08, date: "2017-08-28T19:05:27.121Z", price: "61.25", symbol: "ATVI", name: "Activision Blizzard" },
-                             { change: -0.05, date: "2017-08-28T19:05:27.121Z", price: "46.62", symbol: "INTC", name: "Intel Corp." },
-                             { change: 0.05, date: "2017-08-28T19:05:27.121Z", price: "84.43", symbol: "MSFT", name: "Microsoft Corp." },
-                             { change: -0.85, date: "2017-08-28T19:05:27.121Z", price: "174.76", symbol: "AAPL", name: "Apple Inc." },
-                             { change: -2.4, date: "2017-08-28T19:05:27.121Z", price: "209.25", symbol: "NVDA",
-                                name: "Nvidia Corporation" }];
+        { change: -0.05, date: "2017-08-28T19:05:27.121Z", price: "46.62", symbol: "INTC", name: "Intel Corp." },
+        { change: 0.05, date: "2017-08-28T19:05:27.121Z", price: "84.43", symbol: "MSFT", name: "Microsoft Corp." },
+        { change: -0.85, date: "2017-08-28T19:05:27.121Z", price: "174.76", symbol: "AAPL", name: "Apple Inc." },
+        {
+            change: -2.4, date: "2017-08-28T19:05:27.121Z", price: "209.25", symbol: "NVDA",
+            name: "Nvidia Corporation"
+        }];
 
         // IMPORTANT: Data is mocked because Alphavanted has a limit of 5 requests per minute. So we are sending just mocked data
         res.send(fallbackData);
